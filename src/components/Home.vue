fcd1820252fc4b59eb622bff9a75653c9b3e98ad
Ikaros <873478583@qq.com>
2020-08-24 22:35:24 +0800

    基本完成了add/edit

---
+++ src/components/Home.vue
@@ -0,0 +1,141 @@
+<template>
+  <el-container class="home-container">
+    <!--头部区域-->
+    <el-header>
+      <div class>
+        <img src="../assets/myLogo.png" alt height="56px" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!--页面主体区域-->
+    <el-container>
+      <!--页面侧边栏-->
+      <el-aside :width="isCollapse?'64px':'200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          :unique-opened="false"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
+          <!--一级菜单-->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+            <!--一级菜单模板区域-->
+            <template slot="title">
+              <!--图标-->
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <!--二级菜单-->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!--页面侧内容主体-->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menulist: [],
+      iconsObj: {
+        "125": "iconfont icon-user",
+        "103": "iconfont icon-tijikongjian",
+        "101": "iconfont icon-shangpin",
+        "102": "iconfont icon-danju",
+        "145": "iconfont icon-baobiao",
+      },
+      //是否折叠
+      isCollapse: false,
+      activePath: ''
+    }
+  },
+  created() {
+    this.getMenuList();
+    this.activePath = window.sessionStorage.getItem("activePath")
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get("menus");
+      console.log(res);
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.menulist = res.data;
+    },
+    //点击按钮切换折叠与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse;
+    },
+    saveNavState(activePath) {
+      window.sessionStorage.setItem("activePath", activePath)
+      this.activePath=activePath
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: rgb(5, 168, 168);
+  display: flex;
+  padding-left: 0;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 10px;
+    }
+  }
+}
+.el-aside {
+  background-color: #0c4d5e77;
+  .el-menu {
+    border-right: none;
+  }
+}
+.el-main {
+  background-color: #eaedfe;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button {
+  background-color: #0c4d5e77;
+  font-size: 10px;
+  line-height: 24px;
+  color: #ffffff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css'
+//导入全局样式
+import './assets/css/global.css'
+//导入字体图标
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+axios.defaults.baseURL=`http://127.0.0.1:8888/api/private/v1/`
+axios.interceptors.request.use(config=>{
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+  return config
+})
+Vue.prototype.$http=axios
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
