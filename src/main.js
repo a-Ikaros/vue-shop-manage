d07e5024b367e1b54ec2edf093a768f34f2f1032
Ikaros <873478583@qq.com>
2020-09-08 15:49:07 +0800

    完成了分类功能的开发

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,6 +8,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/global.css'
 //导入字体图标
 import './assets/fonts/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
 import axios from 'axios'
 axios.defaults.baseURL=`http://127.0.0.1:8888/api/private/v1/`
 axios.interceptors.request.use(config=>{
@@ -16,6 +18,8 @@ axios.interceptors.request.use(config=>{
 Vue.prototype.$http=axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table',TreeTable)
 
 new Vue({
   router,
